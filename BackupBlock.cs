0052d0a9684f6e61cc91f73d495321622d56f854
Fluenoriph <fluenoriph@gmail.com>
2025-05-30 00:48:55 +1000

    Tracing start

--- BackupBlock.cs
+++ BackupBlock.cs
@@ -1,43 +1,44 @@
 ﻿using System.Text.RegularExpressions;
+using System.Xml.Linq;
 
 
 namespace BackupBlock
 {
-    struct MonthValues
+    readonly struct MonthValues
     {
-        public Dictionary<string, string> Table { get; } = table;
+        public Dictionary<string, int> Table { get; }
 
-        private static readonly Dictionary<string, string> table = new()
+        private static readonly Dictionary<string, int> table = new()
         {
-            ["Январь"] = "01",
-            ["Февраль"] = "02",
-            ["Март"] = "03",
-            ["Апрель"] = "04",
-            ["Май"] = "05",
-            ["Июнь"] = "06",
-            ["Июль"] = "07",
-            ["Август"] = "08",
-            ["Сентябрь"] = "09",
-            ["Октябрь"] = "10",
-            ["Ноябрь"] = "11",
-            ["Декабрь"] = "12"
+            ["Январь"] = 1,
+            ["Февраль"] = 2,
+            ["Март"] = 3,
+            ["Апрель"] = 4,
+            ["Май"] = 5,
+            ["Июнь"] = 6,
+            ["Июль"] = 7,
+            ["Август"] = 8,
+            ["Сентябрь"] = 9,
+            ["Октябрь"] = 10,
+            ["Ноябрь"] = 11,
+            ["Декабрь"] = 12
         };
 
-        public MonthValues() { }
+        public MonthValues() { Table = table; }
     }
 
 
-    class BackupItem(string rgx_pattern)
+    class BackupItem(string path, string rgx_pattern)
     {
-        private static DateTime current_date = DateTime.Now;
+        private static DateTime current_date = DateTime.Now;   // static if to year ??
         private static string Year = current_date.Year.ToString();
         private static MonthValues monthes = new();
         public string Item_type { get; set; } = "*.pdf";
         public int Items_count { get; set; }
 
-        public List<FileInfo> GetBackupingItems(string month, string path)
+        public List<FileInfo> GetBackupingItems(string month)
         {
-            string date_file_pattern = string.Concat("\\d{2}\\.", monthes.Table[month], "\\.", Year, "\\.", Item_type, "$"); // lambda param. ??
+            string date_file_pattern = string.Concat("\\d{2}\\.", $"0{monthes.Table[month]}", "\\.", Year, "\\.", Item_type, "$"); 
             string full_pattern = string.Concat(rgx_pattern, date_file_pattern);
             Regex rgx = new(full_pattern, RegexOptions.IgnoreCase);
 
@@ -48,10 +49,10 @@ namespace BackupBlock
                                                  where rgx.IsMatch(file.Name)
                                                  select file;
 
-            List<FileInfo> result = [.. backup_block];
-            Items_count = result.Count;
+            List<FileInfo> result_block = [.. backup_block];
+            Items_count = result_block.Count;
 
-            return result;     // status ?  or null files !!!
+            return result_block;     // status ?  or null files !!!
         }
     }
 
@@ -60,27 +61,11 @@ namespace BackupBlock
     {
         private static List<string> protocol_types = ["ф", "фа", "р", "ра", "м", "ма"];   // ключи которые без "а" считать в Уссурийск
         private static string number_capture = "^(?<number>\\d+)-";
-        public Dictionary<string, int> Type_Sums { get; } = [];
-        public static List<string> Missing_Protocols { get; } = []; 
-
         private TypePattern rgx_number = static (type) => new($"{number_capture}{type}-", RegexOptions.IgnoreCase);
 
-        private MissingProtocolNumbers none_numbers = static (names) =>
-        {
-            List<int> numbers = [];
-            foreach (string s in names)
-            {
-                Match match = Regex.Match(s, number_capture);
-                if (match.Success) { numbers.Add(Convert.ToInt32(match.Groups["number"].Value)); }
-            }
-
-            List<int> numbers_range = [];
-            for (int i = numbers.Min(); i <= numbers.Max(); i++) { numbers_range.Add(i); }
-                        
-            return [.. numbers_range.Except(numbers)];
-        };
-           
-        public void CalcProtocolTypes()
+        public Dictionary<string, int> Type_Sums { get; } = [];
+         
+        public void Calculate()
         {
             foreach (string type_i in protocol_types)
             {
@@ -91,37 +76,112 @@ namespace BackupBlock
                 List<string> types = [.. type_block];
                 Type_Sums.Add(type_i, types.Count);
 
-                if (types.Count > 2) { foreach (int x in none_numbers(types)) { Missing_Protocols.Add($"{x}-{type_i}"); } }          
+
+
+
+
+                //if (types.Count > 2) { foreach (int x in none_numbers(types)) { Missing_Protocols.Add($"{x}-{type_i}"); } }          
             }
         }
 
         delegate Regex TypePattern(string protocol_type);
-        delegate List<int> MissingProtocolNumbers(List<string> protocol_names);
+    }
+
+
+    class MissingNumbers
+    {
+        public static List<string> Missing_Protocols { get; } = [];
+
+        public Numbers get_type_numbers = static (x, y) =>
+        {
+            List<int> numbers = [];
+            foreach (string s in x)
+            {
+                Match match = Regex.Match(s, y);
+                if (match.Success) { numbers.Add(Convert.ToInt32(match.Groups["number"].Value)); }
+            }
+            return numbers;
+        };
+
+        public List<string> Calculate(List<string> protocol_type_names, string rgx_capture_pattern) // lambda
+        {
+            
+
+            int max_number = numbers.Max();
+
+
+
+            List<int> numbers_range = [];
+
+
+            for (int i = numbers.Min(); i <= max_number; i++) { numbers_range.Add(i); }  // 'min' read xml max values
+
+
+
+
+            return [.. numbers_range.Except(numbers)];
+        }
+
+        public delegate List<int> Numbers(List<string> protocol_type_names, string rgx_capture_pattern);
     }
 
 
     namespace Logging
     {
         interface ISettings
-        {
-            int Value { get; set; }  // list ???? to types !!
-            string Config_File { get; set; }
-
-            void Read();
-            void Write();
-
-
-            // xml path file свойство
-            // max value of protocol type
-            // methods read write
+        {      
+            void Read(); 
+            void Write(List<int> max_numbers);
         }
 
 
+        class XmlConfig(int month_value) : ISettings
+        {
+            private readonly List<string> items = ["F", "FA", "R", "RA", "M", "MA"];
+            private static string Config_File { get; } = "config.xml";
+            public List<int> Values { get; private set; } = [];
 
-        // xml config 'month max number'
+            private XmlAccess get_config = (x) =>
+            {
+                XDocument xdoc = XDocument.Load(Config_File);
 
+                var max_numbers = xdoc.Element("configuration")?
+                    .Elements("max_numbers")
+                    .FirstOrDefault(p => p.Attribute("month")?.Value == x);
 
+                return max_numbers;
+            };
 
+            void ISettings.Read()
+            {
+                if (month_value is not 1)
+                {
+                    var xvalue = get_config($"{month_value + 1}");      
+                    foreach (var item in items) { Values.Add(Convert.ToInt32(xvalue?.Attribute(item)?.Value)); }                    
+                }
+                else { return; }
+            }
+            
+            void ISettings.Write(List<int> max_numbers)
+            {
+                var xvalue = get_config(month_value.ToString());
 
+                for (int i = 0; i <items.Count; i++)
+                {
+                    int x = max_numbers[i];
+
+                    if (x != 0) 
+                    {
+                        var y = xvalue?.Element(items[i]);
+                        y.Value = x.ToString(); // if == null....
+                    }
+                    else { continue; }
+                }
+            }
+
+            delegate XElement? XmlAccess(string month_value);
+        }
+
+        
     }
 }
